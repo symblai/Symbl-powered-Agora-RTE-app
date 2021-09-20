2c1af10190c41a4c13f8815a0e8985e8b2d34163
sergeym-mac <c_smoraru@zipwhip.com>
2021-09-20 15:28:05 +0300

    add postgres url

--- Symbl-Powered-Agora-Backend-master/models/db.go
+++ Symbl-Powered-Agora-Backend-master/models/db.go
@@ -14,7 +14,7 @@ type Database struct {
 
 // CreateDB is used to initialize a new database connection
 func CreateDB(dbURL string) (*Database, error) {
-	db, err := gorm.Open("postgres", "postgres://postgres:neeraj@localhost:5432/AgoraRTEDB?sslmode=disable")
+	db, err := gorm.Open("postgres", "postgres://ufkfnqnxkzcvnl:285eee312dee11c2282637101e1abab238bba9f86ca9643b063fda4ec21b4d2e@ec2-54-158-247-97.compute-1.amazonaws.com:5432/d2q7nhhmoclhrh")
 	if err != nil {
 		return nil, err
 	}
